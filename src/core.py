45c5091ee88142e0485bfa481311d38252061735
Derek Barbosa <dbarbosa@bu.edu>
2022-04-07 10:00:56 -0400

    finished p2p chat

--- src/core.py
+++ src/core.py
@@ -1,13 +1,13 @@
 from http.client import CONTINUE
 import os
-import string
 import time
 import datetime
 import pyjokes
 import logging
 import socket
-import threading
+import signal
 from . import messaging
+
 
 class UserData:
     def __init__(self, userName, hostIP, filedescriptor):
@@ -35,42 +35,43 @@ def chat_setup(f):
     hostname = socket.gethostname()
     hostIP = socket.gethostbyname(hostname)
 
-    userName = input("Before we get started, please enter a username: ").strip()
+    userName = input(
+        "Before we get started, please enter a username: ").strip()
     logging.debug("Entering Infinite While Loop!")
     while not userName:
         try:
             userName = str(userName)
         except ValueError:
-            logging.error("Exceuption occured. Variable %s was not a valid entry", userName)
+            logging.error(
+                "Exceuption occured. Variable %s was not a valid entry", userName)
             continue
         if userName:
             print("You entered: " + str(userName))
-            break;
+            break
         else:
             print("Error, username not valid. Please enter valid username")
 
-    user = UserData(userName=userName,hostIP=hostIP,filedescriptor=f)
+    user = UserData(userName=userName, hostIP=hostIP, filedescriptor=f)
 
     f.write("FILE CREATED: " + str(datetime.datetime.now()) + "\n")
     f.write("USER-ENTERED 'USERNAME': " + userName + "\n")
     f.write("USER HOST IP: " + hostIP + "\n")
     return user
 
-#messaging() in message.py
+# messaging() in message.py
+
+
 def chat(user1):
-    dudfd = 0;
-    recv_sock, send_sock, tempname, tempIP= messaging.port_setup()
-    user1.fd.write("REMOTE HOSTNAME: " +  user2.name + "\n")
+    dudfd = 0
+    tempname = input("Who are you messaging (what is their hostname)? ")
+    tempIP = input("What is their IP address? ")
+    user2 = UserData(tempname, tempIP, dudfd)
+    user1.fd.write("REMOTE HOSTNAME: " + user2.name + "\n")
     user1.fd.write("REMOTE IP: " + user2.ip + "\n")
     user1.fd.write("**** BEGIN CHAT LOGS **** ")
+    messaging.chatConnection(user1.fd, user1.name, user2.name, user2.ip)
+    return user2
 
-    user2 = UserData(tempname, tempIP, dudfd)
-
-    # set up message receiving in the background on a different thread
-    receive_thread = threading.Thread(target=messaging.receive_messages, args=([recv_sock, user2.name]))
-    receive_thread.start()
-    messaging.send_messages(send_sock, user1.name)   
-    receive_thread.join()
 
 def main_menu():
     os.system('clear')
@@ -78,7 +79,7 @@ def main_menu():
     f = open("chatdata" + str(datetime.datetime.now()) + ".txt", "w+")
 
     print("WELCOME TO OUR CHAT ROOM")
-    print("PLEASE ENJOY YOUR STAY!!")
+    print("SEND YOUR MESSAGES AND HAVE FUN!!")
 
     while True:
         print("************************")
@@ -90,20 +91,20 @@ def main_menu():
         print("*|                    |*")
         print("************************")
 
-        print("All invalid options will be ignored")
+        print("All invalid options will be ignored you donut")
         ui = input("Please select options 1-3 using your keyboard  \n").strip()
         print("\n\n")
         try:
             ui = int(ui)
             print("You entered: " + str(ui))
         except ValueError:
-            logging.error("Exceuption occured. Variable %s was not a valid entry", ui)
+            logging.error(
+                "Exceuption occured. Variable %s was not a valid entry", ui)
             continue
         if ui == 1:
             logging.debug("Succesfully broke from while loop")
             user1 = chat_setup(f)
             user2 = chat(user1)
-            ##connect chat (pass fd and user obj)
             continue
         if ui == 2:
             f.close()
@@ -114,4 +115,3 @@ def main_menu():
             tell_joke()
             print("\n\n")
             continue
-        
--- src/messaging.py
+++ src/messaging.py
@@ -3,23 +3,6 @@ import socket
 import threading
 import datetime
 
-
-# def port_setup():
-#     port_to_send = 9125
-#     ip_to_send = input("IP of person to send to: ")
-#     port_to_listen = 9124
-#     recv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     recv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#     #recv_sock.setblocking(0)
-#     send_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     send_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#     #send_sock.setblocking(0)
-#     recv_sock.bind(('', int(port_to_listen)))
-#     recv_sock.listen(1)
-#     send_sock.connect((ip_to_send, int(port_to_send)))
-#     return recv_sock, send_sock, ip_to_send
-
-
 def receive_messages(chat_fd, remoteUserName):
     # send messages
     port = input("Port to bind to: ")
@@ -28,26 +11,23 @@ def receive_messages(chat_fd, remoteUserName):
     recv_sock.bind(('', int(port)))
     recv_sock.listen(1)
     conn, addr = recv_sock.accept()
-    ##print("Received connection from " + addr[0])
     while True:
-        # conn, addr = recv_sock.accept()
-        # print("Received connection from " + addr[0])
         message_received = conn.recv(1024)
-        print("\n" + message_received.decode())
-        ##print(addr[0] + message_received.decode())
-        # chat_fd.write("\n MESSAGE RECEIVED: " + message_received.decode() +##   " " + str(datetime.datetime.now()) + "\n")
+        if len(message_received.decode()) > 1:
+            if message_received.decode() == "discon_ACK":
+                return
+            else:
+                print("\n" + message_received.decode())
+                chat_fd.write("MESSAGE RECEIVED \n" + " Username: " + message_received.decode() + " at " + str(datetime.datetime.now()) + "\n")
 
 
-def send_messages(chat_fd, localUserName):
+def send_messages(chat_fd, localUserName, remoteUserName, remote_ip):
     # send messages
     s_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    peer_ip = input("Enter IP of peer to send to: ")
-    peer_port = input("Enter port of peer to send to: ")
-    s_sock.connect((peer_ip, int(peer_port)))
+    peer_port = input("Enter port of " + remoteUserName + " peer to send to: ")
+    s_sock.connect((remote_ip, int(peer_port)))
     while True:
-        # peer_ip = input("Enter IP of peer to send to: ")
-        # peer_port = input("Enter port of peer to send to: ")
         message_to_send = input("Enter message: ").replace(
             'b', '').encode('utf-8')
         if message_to_send == ' ':
@@ -55,13 +35,11 @@ def send_messages(chat_fd, localUserName):
         else:
             s_sock.sendall("\n MESSAGE RECEIVED: ".encode('utf-8'))
             s_sock.sendall(bytes(localUserName + ":  ", encoding='utf8') + message_to_send)
-            ##print("Message sent!")
-            # s_sock.shutdown()
-            # chat_fd.write("MESSAGE SENT: " + message_to_send.decode() +
-            # " " + str(datetime.datetime.now()) + "\n")
-            q_code = str(input("Do you wish to continue (Y/N)? "))
-            if q_code == "n" or q_code == "N" or q_code == "no":
-                return
+            chat_fd.write("MESSAGE SENT: \n" + "Username: " + localUserName + message_to_send.decode() + " at " + str(datetime.datetime.now()) + "\n")
+            q_code = str(input("Do you wish to continue (Y/N)?"))
+            if q_code == "n" or q_code == "N" or q_code == "no" or q_code == "quit" or q_code == "exit" or q_code == "stop":
+                s_sock.sendall(bytes("discon_ACK", encoding='utf8'))
+                return 9999
             elif q_code == "y" or q_code == "Y" or q_code == "yes":
                 continue
             else:
@@ -69,22 +47,14 @@ def send_messages(chat_fd, localUserName):
                 continue
 
 
-def main():
+def chatConnection(chatfd, localUserName, remoteUserName, remote_IP):
+    receiveThread = threading.Thread(target=receive_messages,
+                              args=[chatfd, remoteUserName])
 
-    chatfd = open('sample.txt', 'w+')
-    username = input('wats ur username: ')
-    remoteuser = input('who u talkin 2: ')
+    sendThread = threading.Thread(target=send_messages, args=[chatfd, localUserName, remoteUserName, remote_IP])
 
-    dumvar = threading.Thread(target=receive_messages,
-                              args=[chatfd, remoteuser])
-
-    dumvar2 = threading.Thread(target=send_messages, args=[chatfd, username])
-
-    dumvar.start()
-    dumvar2.start()
-    dumvar.join()
-    dumvar2.join()
-
-
-if __name__ == '__main__':
-    main()
+    receiveThread.start()
+    sendThread.start()
+    if sendThread.join() == 9999:
+        print("Chat Disconnected! Goodbye :) ")
+        receiveThread.join()
